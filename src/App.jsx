fecc9f07dc35e0207f2b143c7be8b91b45353cc9
Ufuk İncesu <91136022+Scoplex@users.noreply.github.com>
2023-11-18 17:35:28 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,143 @@
+import "./App.css";
+import React from 'react';
+
+
+
+function Arama({aramaMetni, onSearch, }){
+ 
+  function handleChange(event){
+    setAramaMetni(event.target.value)
+     props.onSearch(event);
+     localStorage.setItem("aranan", event.target.value); 
+  }
+  React.useEffect(() => {
+    localStorage.setItem("aranan", aramaMetni); 
+  },[aramaMetni]);
+    return(
+    <div>     
+      <label htmlFor="arama">Ara: </label>
+      <input id="arama" type="text" onChange={onSearch} value={aramaMetni}   />  
+      <p>
+        
+      </p>
+      </div>
+  )
+}
+
+
+
+function Yazi({id, url, baslik, yazar, yorum_sayisi, puan}){
+  return(
+    <li key={id}>
+          <span>
+            <a href={url}>{baslik}</a>, 
+          </span>
+          <span><b>Yazar:</b> {yazar}, </span>
+          <span><b>Yorum Sayısı:</b> {yorum_sayisi}, </span>
+          <span><b>Puan:</b> {puan}</span>
+        </li>
+  )
+}
+
+
+
+
+
+function Liste(props) {
+  return(
+  <ul>
+   {props.yazilar.map(function (yazi){
+    return (
+    
+    <Yazi key={yazi.id} {...yazi}/>
+    );
+   })}
+
+  </ul>
+
+  )                     
+}
+
+
+
+
+
+function App() {
+  const [aramaMetni, setAramaMetni] = React.useState(localStorage.getItem("aranan") ||  "React");
+
+  const yaziListesi = [
+    {  
+      baslik: "React Öğreniyorum",
+      url: "www.sdu.edu.tr",
+      yazar: "Recep Ufuk İncesu",
+      yorum_sayisi: 198,
+      puan: 5,
+      id: 0,
+    },
+    {
+      baslik: "Web Teknolojileri ve Programlama",
+      url: "wwww.google.com.tr",
+      yazar: "Asım Yüksel",
+      yorum_sayisi: 2,
+      puan: 5,
+      id: 1,
+    },
+
+    {
+      baslik: "Python Programlama Temelleri",
+      url: "www.python.org",
+      yazar: "atıl Samancıoğlu",
+      yorum_sayisi: 10,
+      puan: 4.5,
+      id: 2
+    },
+
+    {
+      baslik: "Unity ile Mobil Oyun Yapımı",
+      url: "https://ufuk-incesu.vercel.app/",
+      yazar: "Recep Ufuk İncesu",
+      yorum_sayisi: 128,
+      puan: 5.0,
+      id: 3
+    },
+
+    {
+      baslik: "Unity nedir? Unity'nin Avantajları",
+      url: "www.canakandemir.com/",
+      yazar: "Can Akan Demir",
+      yorum_sayisi: 12,
+      puan: 4.7,
+      id: 4
+    },
+    
+  ];
+
+  const arananYazilar = yaziListesi.filter(function (yazi) {
+    return (
+      yazi.baslik.toLowerCase().includes(aramaMetni.toLowerCase()) || yazi.yazar.toLowerCase().includes(aramaMetni.toLowerCase()) 
+    );
+  });
+
+
+
+
+  function handleSearch(event){
+   setAramaMetni(event.target.value);
+  }
+
+  React.useEffect(()=>{
+    localStorage.setItem("aranan",aramaMetni);
+  },[aramaMetni]);
+
+  return (
+    <div>
+      <h1>Yazılar</h1>
+      <Arama aramaMetni={aramaMetni}  onSearch = {handleSearch} />  
+      <strong>{aramaMetni} araniyor...</strong>
+      <hr />
+      <Liste yazilar={arananYazilar}/>
+    </div>
+  );
+
+}
+export default App;
